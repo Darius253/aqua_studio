82f33035ca7c6da27d0995a88d5863fad2b8a56d
Othniel Ussher <othnielussher16@gmail.com>
2022-06-12 06:34:20 +0000

    🔨 Fix footer and overflow bugs

--- lib/home/menu/menu_web.dart
+++ lib/home/menu/menu_web.dart
@@ -1,105 +1,71 @@
 import 'package:aqua_studio/shared/exports.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class MenuWeb extends StatefulWidget {
-  final int index;
-  final VoidCallback onClose;
-  final Function scrollToIndex;
-  const MenuWeb(
-      {Key? key,
-      required this.index,
-      required this.onClose,
-      required this.scrollToIndex})
-      : super(key: key);
+  const MenuWeb({Key? key}) : super(key: key);
 
   @override
   State<MenuWeb> createState() => _MenuWebState();
 }
 
 class _MenuWebState extends State<MenuWeb> {
+  final ViewController viewController = Get.find();
+  final List<String> pages = ['SHOWREEL', 'OUR HOUSE', 'CONTACT'];
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 100.0,
-        horizontal: 100,
-      ),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        GestureDetector(
-            onTap: () {
-              widget.scrollToIndex(0);
-              widget.onClose();
-            },
-            child: Text('SHOWREEL',
-                style: TextStyle(
-                    fontFamily: 'Cocomat Ultra',
-                    fontSize: 50,
-                    color: widget.index == 0
-                        ? const Color.fromARGB(255, 70, 69, 69)
-                        : Colors.white))),
-        const SizedBox(
-          height: 60,
-        ),
-        GestureDetector(
-          onTap: () {
-            widget.scrollToIndex(1);
-            widget.onClose();
-          },
-          child: Text(
-            'OUR HOUSE',
-            style: TextStyle(
-                fontFamily: 'Cocomat Ultra',
-                fontSize: 50,
-                color: widget.index == 1
-                    ? const Color.fromARGB(255, 70, 69, 69)
-                    : Colors.white),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 100.0,
+                horizontal: 100,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(pages.length, (index) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      InkWell(
+                        onTap: () async {
+                          viewController.isMenuOpen.value = false;
+                          viewController.currentViewIndex.value = index;
+                          Get.back();
+                          await Future.delayed(
+                            const Duration(milliseconds: 300),
+                          );
+                          viewController.itemScrollController.scrollTo(
+                            index: index,
+                            curve: Curves.easeInOutCubic,
+                            duration: const Duration(milliseconds: 2000),
+                          );
+                        },
+                        child: Text(
+                          pages[index],
+                          style: TextStyle(
+                            fontSize: 50.0,
+                            fontFamily: 'Cocomat Ultra',
+                            color:
+                                index == viewController.currentViewIndex.value
+                                    ? const Color.fromARGB(255, 70, 69, 69)
+                                    : Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 60.0),
+                    ],
+                  );
+                }),
+              ),
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-        GestureDetector(
-            onTap: () {
-              widget.scrollToIndex(2);
-            },
-            child: Text('CONTACT',
-                style: TextStyle(
-                    fontFamily: 'Cocomat Ultra',
-                    fontSize: 50,
-                    color: widget.index == 2
-                        ? const Color.fromARGB(255, 70, 69, 69)
-                        : Colors.white))),
-        const SizedBox(
-          height: 150,
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Home())),
-              child: SvgPicture.asset(
-                'images/Black-04.svg',
-                color: Colors.white,
-                width: 480,
-              ),
-            ),
-            const Expanded(child: SizedBox()),
-            InkWell(
-              onTap: () => widget.onClose(),
-              child: const Text(
-                'CLOSE',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontFamily: 'Macaria',
-                    fontWeight: FontWeight.w700),
-              ),
-            ),
-          ],
-        )
-      ]),
+          const NavBar(),
+        ],
+      ),
     );
   }
 }
