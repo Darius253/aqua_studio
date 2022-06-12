af62f11c87063a664091c59d0ed91ca7c657636c
Darius <68332672+Darius253@users.noreply.github.com>
2022-06-12 17:05:11 +0000

    Removed deleted files and fix code

--- lib/home/contact/contact_mobile.dart
+++ lib/home/contact/contact_mobile.dart
@@ -9,23 +9,129 @@ class ContactMobile extends StatefulWidget {
 }
 
 class _ContactMobileState extends State<ContactMobile> {
-  int pageIndex = 0;
+  final ViewController viewController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.black,
-        body: Stack(
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(
-                top: 40,
-                left: 20.0,
-              ),
-              child: TopTitle(text: 'CONTACT'),
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(top: 50.0, left: 30),
+          child: TopTitle(text: 'CONTACT'),
+        ),
+        VisibilityDetector(
+          key: const Key('contact'),
+          onVisibilityChanged: (visibilityInfo) {
+            if (visibilityInfo.visibleFraction > 0.2) {
+              viewController.currentView.value = Views.contact;
+              viewController.currentViewIndex.value = 2;
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+              left: 30.0,
+              right: 20.0,
             ),
-            ContactFirstScreen()
-          ],
-        ));
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    'REQUEST SERVICE',
+                    style: TextStyle(
+                      fontFamily: 'Cocomat Ultra',
+                      fontSize: 25,
+                      color: Color.fromARGB(255, 230, 226, 226),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        MobileTextBox(
+                            color: Color.fromARGB(255, 172, 168, 168),
+                            hintText: 'TYPE YOUR MESSAGE HERE'),
+                        SizedBox(height: 10),
+                        MobileTextBox(
+                            color: Color.fromARGB(255, 172, 168, 168),
+                            hintText: 'FULL NAME | ORGANISATION NAME'),
+                        SizedBox(height: 10),
+                        MobileTextBox(
+                            color: Color.fromARGB(255, 172, 168, 168),
+                            hintText: 'E-MAIL'),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Button()
+                      ],
+                    ),
+                  ]),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'YOUTUBE',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontFamily: 'Cocomat Ultra',
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 231, 225, 225),
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'VIMEO',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontFamily: 'Cocomat Ultra',
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 233, 227, 227),
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'INSTAGRAM',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontFamily: 'Cocomat Ultra',
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 238, 232, 232),
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'DRIBBLE',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontFamily: 'Cocomat Ultra',
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 241, 235, 235),
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                ]),
+          ),
+        ),
+      ],
+    );
   }
 }
