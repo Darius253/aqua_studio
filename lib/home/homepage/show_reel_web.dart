86029e3e817316903cbda949a8bcb863b672b349
Othniel Ussher <othnielussher16@gmail.com>
2022-06-12 06:34:11 +0000

    🔨 Fix footer and overflow bugs

--- lib/home/homepage/show_reel_web.dart
+++ lib/home/homepage/show_reel_web.dart
@@ -1,3 +1,4 @@
+import 'package:aqua_studio/shared/exports.dart';
 import 'package:flutter/material.dart';
 
 class ShowReelWeb extends StatefulWidget {
@@ -8,19 +9,29 @@ class ShowReelWeb extends StatefulWidget {
 }
 
 class _ShowReelWebState extends State<ShowReelWeb> {
+  final ViewController viewController = Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return VisibilityDetector(
+      key: const Key('show reel'),
+      onVisibilityChanged: (visibilityInfo) {
+        if (visibilityInfo.visibleFraction > 0.2) {
+          viewController.currentView.value = Views.reel;
+          viewController.currentViewIndex.value = 0;
+        }
+      },
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 80.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 300,
-              color: Colors.grey,
-            ),
-          ],
+        padding: const EdgeInsets.only(
+          top: 80.0,
+          left: 80.0,
+          right: 80.0,
+        ),
+        child: Image.asset(
+          Images.reel,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: Get.height * 0.7,
         ),
       ),
     );
