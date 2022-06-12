201073e2e71b5186566fd34a58a149ed1e07ee08
Othniel Ussher <othnielussher16@gmail.com>
2022-06-12 06:34:18 +0000

    🔨 Fix footer and overflow bugs

--- lib/home/menu/menu_mobile.dart
+++ lib/home/menu/menu_mobile.dart
@@ -1,6 +1,5 @@
 import 'package:aqua_studio/shared/exports.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class MenuMobile extends StatefulWidget {
   final int index;
