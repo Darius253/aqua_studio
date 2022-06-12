f67d7c6b6ff76ac43c10df556e0d2dc523ff1095
Othniel Ussher <othnielussher16@gmail.com>
2022-06-12 06:35:18 +0000

    🔨 Fix footer and overflow bugs

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -1,9 +1,20 @@
+// pub
+export 'package:get/get.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:delayed_display/delayed_display.dart';
+export 'package:visibility_detector/visibility_detector.dart';
+export 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+
+// controllers
+export 'package:aqua_studio/controllers/view_controller.dart';
+
+// local
 export 'package:aqua_studio/home/homepage/show_reel.dart';
 export 'package:aqua_studio/widgets/responsiveness.dart';
 export 'package:aqua_studio/home/splashscreen.dart';
 export 'package:aqua_studio/home/menu/menu.dart';
 export 'package:aqua_studio/home/ourhouse/our_house_.dart';
-export 'package:aqua_studio/widgets/bottom _items.dart';
+export 'package:aqua_studio/widgets/nav_bar.dart';
 export 'package:aqua_studio/home/ourhouse/firstscreen.dart';
 export 'package:aqua_studio/home/ourhouse/secondscreen.dart';
 export 'package:aqua_studio/home/ourhouse/thirdscreen.dart';
@@ -23,3 +34,4 @@ export 'package:aqua_studio/home/homepage/show_reel_web.dart';
 export 'package:aqua_studio/home/homepage/show_reel_mobile.dart';
 export 'package:aqua_studio/home/homepage/home.dart';
 export 'package:aqua_studio/home/contact/widgets/mobile_textbox.dart';
+export 'package:aqua_studio/shared/constants.dart';
