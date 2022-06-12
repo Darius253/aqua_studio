ed6f812d60080c9f1790de5afe6b6d24d09c4744
Othniel Ussher <othnielussher16@gmail.com>
2022-06-12 06:34:09 +0000

    🔨 Fix footer and overflow bugs

--- lib/home/homepage/home.dart
+++ lib/home/homepage/home.dart
@@ -12,100 +12,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final PageController _pageController = PageController();
   int pageIndex = 0;
-  String currentPage = '';
   bool openMenu = false;
   List<Widget> pages = [
     const ShowReel(),
     const OurHousePage(),
     const ContactPage(),
   ];
+
+  final ItemPositionsListener itemPositionsListener =
+      ItemPositionsListener.create();
+
+  late final ViewController viewController;
+
   @override
   void initState() {
     super.initState();
-    currentPage = pageNames[0];
+    Get.put(ViewController());
+    viewController = Get.find();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.black,
-        body: Stack(
-          children: [
-            Column(
-              children: [
-                Expanded(
-                  child: ReponsiveWidget(
-                    largeScreen: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 600,
-                        left: 80,
-                        right: 80,
-                      ),
-                      child: BottomItems(
-                        scrollToIndex: (pageIndex) => widget.scrollToIndex!(0),
-                        text: currentPage,
-                        openMenu: () => setState(() => openMenu = true),
-                      ),
-                    ),
-                    mediumScreen: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 450,
-                        left: 80,
-                        right: 80,
-                      ),
-                      child: BottomItems(
-                        scrollToIndex: (pageIndex) => widget.scrollToIndex!(0),
-                        text: currentPage,
-                        openMenu: () => setState(() => openMenu = true),
-                      ),
-                    ),
-                    smallScreen: Padding(
-                      padding: const EdgeInsets.only(top: 550),
-                      child: BottomMobileItems(
-                        scrollToIndex: (pageIndex) => widget.scrollToIndex!(0),
-                        icon: Icons.menu,
-                        openMenu: () => setState(() => openMenu = true),
-                      ),
-                    ),
-                  ),
-                )
-              ],
+      body: Column(
+        children: [
+          Expanded(
+            child: ScrollablePositionedList.builder(
+              shrinkWrap: true,
+              itemCount: pages.length,
+              itemBuilder: (context, index) => pages[index],
+              itemPositionsListener: itemPositionsListener,
+              itemScrollController: viewController.itemScrollController,
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).size.height / 5),
-              child: PageView(
-                scrollDirection: Axis.vertical,
-                pageSnapping: false,
-                onPageChanged: (index) {
-                  setState(() {
-                    pageIndex = index;
-                    currentPage = pageNames[index];
-                  });
-                },
-                controller: _pageController,
-                children: pages,
+          ),
+          ReponsiveWidget(
+            largeScreen: const NavBar(),
+            mediumScreen: const NavBar(),
+            smallScreen: Padding(
+              padding: const EdgeInsets.only(top: 550),
+              child: BottomMobileItems(
+                scrollToIndex: (pageIndex) => widget.scrollToIndex!(0),
+                icon: Icons.menu,
+                openMenu: () => setState(() => openMenu = true),
               ),
             ),
-            AnimatedPositioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              duration: const Duration(milliseconds: 300),
-              top: openMenu ? 0 : MediaQuery.of(context).size.height,
-              child: MenuScreen(
-                index: pageIndex,
-                onClose: () => setState(() => openMenu = false),
-                scrollToIndex: (pageIndex) => _pageController.animateTo(
-                    pageIndex,
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.linear),
-              ),
-            )
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
