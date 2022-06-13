ea2d3e58d094ac4bf5ed2c81a6217c2149326638
Othniel Ussher <othnielussher16@gmail.com>
2022-06-13 14:54:20 +0000

    🔨 FIx menu not scrolling on tab tap

--- lib/controllers/view_controller.dart
+++ lib/controllers/view_controller.dart
@@ -1,9 +1,17 @@
 import 'package:aqua_studio/shared/exports.dart';
+import 'package:flutter/material.dart';
 
 class ViewController extends GetxController {
   var currentView = Views.reel.obs;
   var currentViewIndex = 0.obs;
   var isMenuOpen = false.obs;
+  final PageController pageController = PageController();
 
-  final ItemScrollController itemScrollController = ItemScrollController();
+  // final ItemScrollController itemScrollController = ItemScrollController();
+  
+  @override
+  void onClose() {
+    pageController.dispose();
+    super.onClose();
+  }
 }
